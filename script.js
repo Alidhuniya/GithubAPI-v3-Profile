d0059d4f2938effac1fe5de0f3c49e7946c8d3a8
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-07-08 21:10:32 -0700

    left section completed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // left section
 
 // img
-const leftSectionImg = async () => {
+const leftSection = async () => {
 
     const url = "https://api.github.com/users/Alidhuniya";
     const get = await fetch(url);
@@ -11,11 +11,39 @@ const leftSectionImg = async () => {
     // get user img
     document.querySelector(".left__img").src = ` ${data.avatar_url}`;
 
-  
+    // get name
+    document.querySelector(".left__name").innerHTML = `${data.name}`;
+
+    // get github user name
+    document.querySelector(".left__gitUser").innerHTML = `${data.login}`;
+
+    // get bio
+    document.querySelector(".left__bio").innerHTML = `${data.bio}`;
+
+    // get company
+    document.querySelector(".left__company").innerHTML = `${data.company}`;
+
+    // location
+    document.querySelector(".left__location").innerHTML = `${data.location}`;
+
+    // get blog
+    document.querySelector(".left__blog").href = `${data.blog}`;
+
+    // get twitter username
+    document.querySelector(".left__username").href = ` ${"https://twitter.com/" + data.twitter_username}`;
+
+    // get followers
+    document.querySelector(".left__followme").innerHTML = `${data.followers + "followers"}`;
+
+    // get following
+    document.querySelector(".left__iamfollowing").innerHTML = `${data.following + "following"}`;
+
+    // get public repo
+    document.querySelector(".left__publicRepo").innerHTML = `${data.public_repos + "repos"}`;
 
 }
 
-leftSectionImg();
+leftSection();
 
 // icon
 const leftSectionIcon = async () => {
